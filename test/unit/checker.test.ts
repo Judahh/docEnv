5792df32d1ce3b5d9720e7d42cd0e1d46a24816d
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-09-02 16:51:51 -0300

    fix tests

--- test/unit/checker.test.ts
+++ test/unit/checker.test.ts
@@ -26,29 +26,46 @@ test('Test Ternary Basic', async () => {
   expect(ternary).toMatchObject({ if: 'a', then: 'b', else: 'c' });
 });
 
-// test('Test Ternary Large', async () => {
-//   let ternary = Checker.checkOptions('a ? b? c: d : e');
-//   expect(ternary).toMatchObject({ if: 'a', then: 'b', else: 'c' });
-//   ternary = Checker.checkOptions('a ? b : c ? d : e;');
-//   expect(ternary).toMatchObject({ if: 'a', then: 'b', else: 'c' });
-// });
-
-// test('Test Ternary Large with groups', async () => {
-//   let ternary = Checker.checkOptions('a ? (b?c:d) : e;');
-//   expect(ternary).toMatchObject({ if: 'a', then: 'b', else: 'c' });
-//   ternary = Checker.checkOptions('a ? b : (c ? d : e);');
-//   expect(ternary).toMatchObject({ if: 'a', then: 'b', else: 'c' });
-//   ternary = Checker.checkOptions('(a ? b : c) ? d : e;');
-//   expect(ternary).toMatchObject({ if: 'a', then: 'b', else: 'c' });
-// });
-
-test('Test Options Basic', async () => {
-  let options = Checker.checkOptions('a || b || c || d && e');
-  expect(options).toMatchObject({ or: ['a', 'b', 'c', { and: ['d', 'e'] }] });
-  options = Checker.checkOptions('a || b || c || d || e');
-  expect(options).toMatchObject({ or: ['a', 'b', 'c', 'd', 'e'] });
-  options = Checker.checkOptions('a && b && c && d || e');
-  expect(options).toMatchObject({ or: [{ and: ['a', 'b', 'c', 'd'] }, 'e'] });
-  options = Checker.checkOptions('a && b && c && d && e');
-  expect(options).toMatchObject({ or: [{ and: ['a', 'b', 'c', 'd', 'e'] }] });
+test('Test Ternary Large', async () => {
+  let ternary = Checker.checkOptions('a ? b ? c : d : e');
+  expect(ternary).toMatchObject({
+    if: 'a',
+    then: { if: 'b', then: 'c', else: 'd' },
+    else: 'e',
+  });
+  ternary = Checker.checkOptions('a ? (b ? c : d) : e;');
+  expect(ternary).toMatchObject({
+    if: 'a',
+    then: { if: 'b', then: 'c', else: 'd' },
+    else: 'e',
+  });
+  ternary = Checker.checkOptions(' a ? b : c ? d : e');
+  expect(ternary).toMatchObject({
+    if: 'a',
+    then: 'b',
+    else: { if: 'c', then: 'd', else: 'e' },
+  });
+  ternary = Checker.checkOptions('(a ? b : c) ? d : e');
+  expect(ternary).toMatchObject({
+    if: { if: 'a', then: 'b', else: 'c' },
+    then: 'd',
+    else: 'e',
+  });
+  ternary = Checker.checkOptions('a ? b : (c ? d : e)');
+  expect(ternary).toMatchObject({
+    if: 'a',
+    then: 'b',
+    else: { if: 'c', then: 'd', else: 'e' },
+  });
 });
+
+// test('Test Options Basic', async () => {
+//   let options = Checker.checkOptions('a || b || c || d && e');
+//   expect(options).toMatchObject({ or: ['a', 'b', 'c', { and: ['d', 'e'] }] });
+//   options = Checker.checkOptions('a || b || c || d || e');
+//   expect(options).toMatchObject({ or: ['a', 'b', 'c', 'd', 'e'] });
+//   options = Checker.checkOptions('a && b && c && d || e');
+//   expect(options).toMatchObject({ or: [{ and: ['a', 'b', 'c', 'd'] }, 'e'] });
+//   options = Checker.checkOptions('a && b && c && d && e');
+//   expect(options).toMatchObject({ and: ['a', 'b', 'c', 'd', 'e'] });
+// });
