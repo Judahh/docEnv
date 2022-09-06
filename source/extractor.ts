795ddf53c06fb20f51aa029622a388482148e972
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-09-06 09:57:11 -0300

    update

--- source/extractor.ts
+++ source/extractor.ts
@@ -52,7 +52,7 @@ class Extractor {
     startTypes = [...startTypes];
     endTypes = [...endTypes];
 
-    console.log('Bundler S:', string);
+    // console.log('Bundler S:', string);
 
     for (let index = 0; index < string.length; index++) {
       const element = string[index];
@@ -66,7 +66,9 @@ class Extractor {
         hideIndex > -1
       ) {
         const pg = string.substring(index);
+        // console.log('pg:', pg);
         const g = hideFunction(pg);
+        // console.log('g:', g);
         index += (g?.length || 1) - 1;
         continue;
       }
@@ -86,7 +88,7 @@ class Extractor {
       }
 
       if (stack.length === 0 && (match?.length || 0) > 0) {
-        console.log('Bundler:', string, '-', match, '-', starts, '-', ends);
+        // console.log('Bundler:', string, '-', match, '-', starts, '-', ends);
         return Extractor.cleaner(
           string,
           cleanFunction,
@@ -106,7 +108,7 @@ class Extractor {
       //   match.push(endTypes[0]);
       //   string += endTypes[0];
       // }
-      console.log('Bundler:', string, '-', match, '-', starts, '-', ends);
+      // console.log('Bundler:', string, '-', match, '-', starts, '-', ends);
       return Extractor.cleaner(
         string,
         cleanFunction,
@@ -122,7 +124,7 @@ class Extractor {
     ends.push(string.length);
     match.push(endTypes[0]);
     string += endTypes[0];
-    console.log('Bundler L:', string, '-', match, '-', starts, '-', ends);
+    // console.log('Bundler L:', string, '-', match, '-', starts, '-', ends);
     return Extractor.cleaner(
       string,
       cleanFunction,
@@ -153,6 +155,7 @@ class Extractor {
     removeOuter?: boolean,
     object = {}
   ) {
+    // console.log('Cleaner:', string);
     if (
       starts != undefined &&
       ends != undefined &&
@@ -185,7 +188,7 @@ class Extractor {
         }
       }
     }
-    return string;
+    return Extractor.getValue(string);
   }
 
   public static cleanBundle(
@@ -194,10 +197,22 @@ class Extractor {
     end: number,
     removeOuter?: boolean
   ) {
+    // console.log('CleanBundle:', string);
     string = string
       .trim()
       .slice(start + (removeOuter ? 1 : 0), end + 1 - (removeOuter ? 1 : 0));
-    return string;
+    return Extractor.getValue(string);
+  }
+
+  public static getValue(value: string) {
+    // console.log('Value:', value);
+    if (value == undefined) return value;
+    try {
+      return JSON.parse(value);
+    } catch (error) {
+      // console.log('Value Error:', value);
+      return value;
+    }
   }
 
   public static cleanObject(
@@ -206,9 +221,10 @@ class Extractor {
     end: number,
     object = {}
   ) {
+    // console.log('Clean Object:', string, start, end);
     const objectString = string.substring(start + 1, end).trim();
-    const openBrackets = Extractor.openBrackets + Extractor.openObject;
-    const closeBrackets = Extractor.closeBrackets + Extractor.closeObject;
+    const openBrackets = Extractor.openBrackets;
+    const closeBrackets = Extractor.closeBrackets;
 
     const hideFunction = (string) =>
       Extractor.bundler(
@@ -218,12 +234,15 @@ class Extractor {
         closeBrackets
       );
 
-    console.log('cleanObject init', objectString, objectString.length);
+    // console.log('cleanObject init', objectString, objectString.length);
+
+    const openRegex = [...openBrackets].map((s) => '\\' + s).join('');
+    const closeRegex = [...closeBrackets].map((s) => '\\' + s).join('');
 
     let index = 0;
     while (index < objectString.length) {
       const currentString = objectString.substring(index).trim();
-      console.log('cleanObject start', currentString, currentString.length);
+      // console.log('cleanObject start', currentString, currentString.length);
       const bundled = Extractor.bundler(
         currentString,
         Extractor.cleanBundle,
@@ -232,44 +251,50 @@ class Extractor {
         Extractor.openBrackets + Extractor.closeBrackets,
         hideFunction
       ).trim();
-      console.log('cleanObject bundled', bundled);
+      // console.log('cleanObject bundled', bundled);
       const position = objectString.indexOf(bundled, index);
-      console.log('cleanObject position', position);
+      // console.log('cleanObject position', position);
       const name = objectString.substring(index, position).trim();
+
       const toRemove = RegExp(
-        `[${Extractor.propertyEquals + Extractor.propertyNext}]`,
+        `[${
+          Extractor.propertyEquals + Extractor.propertyNext
+        }]+(?![^${openRegex}]*[${closeRegex}])`,
         'g'
       );
+      // console.log('cleanObject bundled', bundled);
       const value = bundled?.replace(toRemove, '')?.trim();
-      object[name] = value;
+      // console.log('cleanObject value', value);
+      object[name] = Extractor.extract(value);
+      // console.log('cleanObject current', object);
 
-      console.log(
-        'cleanObject index',
-        index,
-        '-',
-        position,
-        '-',
-        name,
-        '-',
-        value,
-        '-',
-        bundled
-      );
+      // console.log(
+      //   'cleanObject index',
+      //   index,
+      //   '-',
+      //   position,
+      //   '-',
+      //   name,
+      //   '-',
+      //   value,
+      //   '-',
+      //   bundled
+      // );
       const lastIndex = index;
       index = position + bundled.length;
       if (index === lastIndex) break;
-      console.log(
-        'cleanObject index',
-        index,
-        '-',
-        objectString,
-        '-',
-        objectString.length,
-        '-',
-        object
-      );
+      // console.log(
+      //   'cleanObject index',
+      //   index,
+      //   '-',
+      //   objectString,
+      //   '-',
+      //   objectString.length,
+      //   '-',
+      //   object
+      // );
     }
-    console.log('cleanObject', object);
+    // console.log('cleanObject', object);
     return object;
   }
 
@@ -279,6 +304,7 @@ class Extractor {
     end: number //,
     // removeOuter?: boolean
   ) {
+    // console.log('Clean Ternary:', string);
     const ifEl = string.substring(0, start).trim();
     const thenEl = string.substring(start + 1, end).trim();
     const elseEl = string
@@ -304,6 +330,8 @@ class Extractor {
 
     if (string == undefined) return string;
 
+    // console.log('Extract:', string);
+
     const tempBundle = Extractor.bundler(
       string,
       Extractor.cleanBundle,
@@ -312,6 +340,7 @@ class Extractor {
     );
 
     const isBundle = tempBundle == string;
+    // console.log('Extract b:', string);
 
     string = isBundle
       ? Extractor.bundler(
@@ -329,14 +358,18 @@ class Extractor {
     let minElementIndex;
     for (let index = 0; index < elements.length; index++) {
       const element = elements[index];
-      const newI = string.indexOf(element);
+      const newI = string?.indexOf
+        ? string.indexOf(element)
+        : element == string
+        ? 0
+        : -1;
       if (newI != -1 && (min > newI || min == Infinity)) {
         min = newI;
         minElementIndex = index;
       }
     }
 
-    console.log('min', minElementIndex, '-', min, '-', string, '-', elements);
+    // console.log('min:', minElementIndex, '-', min, '-', string, '-', elements);
 
     if (minElementIndex == undefined) {
       const bundle = Extractor.bundler(
@@ -369,7 +402,7 @@ class Extractor {
           )
       ); // '?' or ':'
 
-    console.log('extract', string);
+    // console.log('extract preObject', string);
 
     return Extractor.bundler(
       string,
