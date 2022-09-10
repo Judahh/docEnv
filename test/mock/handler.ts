fa5185a230e6183b92727e9526ba68a2844195b1
Judah <judahholanda7@gmail.com>
2022-09-10 15:12:53 -0300

    add handler mock

---
+++ test/mock/handler.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+const journaly = {};
+const readDatabase = {};
+const eventDatabase = {};
+
+const MSSQL = class {
+  constructor(..._b: unknown[]) {}
+};
+
+const DAOPersistence = class {
+  constructor(..._b: unknown[]) {}
+};
+
+const ServiceHandler = class {
+  constructor(..._b: unknown[]) {}
+};
+
+const MongoPersistence = class {
+  constructor(..._b: unknown[]) {}
+};
+
+const Handler = class {
+  constructor(..._b: unknown[]) {}
+};
+
+const DatabaseHandler = class {
+  constructor(..._b: unknown[]) {}
+  public static async getInstance(..._b: unknown[]) {}
+};
--- test/sampleAPI/dBHandler.ts
+++ test/sampleAPI/dBHandler.ts
@@ -1,37 +1,6 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-empty-function */
 import AService from './services/aService';
 import { ADAO } from './dAOs/aDAO';
 import BDAO from './dAOs/bDAO';
-
-const journaly = {};
-const readDatabase = {};
-const eventDatabase = {};
-
-const MSSQL = class {
-  constructor(..._b: unknown[]) {}
-};
-
-const DAOPersistence = class {
-  constructor(..._b: unknown[]) {}
-};
-
-const ServiceHandler = class {
-  constructor(..._b: unknown[]) {}
-};
-
-const MongoPersistence = class {
-  constructor(..._b: unknown[]) {}
-};
-
-const Handler = class {
-  constructor(..._b: unknown[]) {}
-};
-
-const DatabaseHandler = class {
-  constructor(..._b: unknown[]) {}
-  public static async getInstance(..._b: unknown[]) {}
-};
 
 const mssql = new MSSQL(readDatabase);
 
